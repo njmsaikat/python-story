9c3086ccec6b02dd18da19631104008113c6be14
njmsaikat <njmsaikat@gmail.com>
2023-01-07 11:47:26 +0600

    reverse, change elements and different step slicing using python slicing

--- programiz (python best practices)/slicing_list.py
+++ programiz (python best practices)/slicing_list.py
@@ -1,4 +1,4 @@
-numbers = [5, 10, 15, 20, 25]
+numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 
 # Slicing using indexes
 first_three_elements = numbers[0:3]  # First inclusive : second exclusive
@@ -21,3 +21,21 @@ print("From element three to last: ", three_to_last)
 
 till_mentioned_no_of_item = numbers[:3]
 print("From first till mentioned: ", till_mentioned_no_of_item)
+
+# Slicing with step (list[start, end, step]) **default step 1
+
+slice_with_step_two = numbers[1:6:2]
+print("2 step interval sliced list(ex end 6): ", slice_with_step_two)
+
+slice_with_step_three = numbers[1:8:3]
+print("3 step sliced list(ex end 8): ", slice_with_step_three)
+
+# Reverse a list using slicing
+
+reversed_list = numbers[::-1]
+print("Reversed list: ", reversed_list)
+
+# Changing multiple list items
+
+numbers[:3] = [-1, -2, -3]
+print("First three elements changed: ", numbers)
